14bb86f816e1203c3458933d19e8647a58d25d39
BullWooStar <ccatal66@naver.com>
2022-11-27 11:50:45 +0900

    feat: pdf버튼생성

--- UI&Layout/PdfDownLoadButton.js
+++ UI&Layout/PdfDownLoadButton.js
@@ -1,10 +1,12 @@
+import styles from "./PdfDownLoadButton.module.css";
+
 function PdfDownLoadButton() {
   return (
-    <>
+    <button className={styles.button}>
       <a href="../public/static/resume.pdf" download>
-        PDF 다운로드
+        PDF
       </a>
-    </>
+    </button>
   );
 }
 
